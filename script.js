a3884163d604912d89e9693e58593e2ecd05b2c9
Exotic P <sjwiegman@gmail.com>
2022-08-25 19:49:39 -0400

    added 9-5 table in HTML with unique class ids

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-var date = moment().format("[Today is] MMMM Do, YYYY");
+var date = moment().format("[Today is] MMMM Do, YYYY"); 
 console.log(date);
-$("#currentDay").text(date);
+$("#currentDay").text(date); //Logs todays date
+
+
